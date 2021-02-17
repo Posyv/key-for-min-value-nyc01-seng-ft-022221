864f717624d4b04e4034f86dcfdeaed7779a7c42
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-17 23:02:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.sort_by{|key, value| value}.first
+  least = 0
+if key_for_min_value|key, value| < 0
+return key_for_min_value
+least += 1
 end
