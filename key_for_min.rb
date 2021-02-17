528413488fc7184ba95e57e52c4bd1f2d299666d
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-17 23:18:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   least = 0
 name_hash.each do
-  if key_for_min_value{|key, value|} < 0
+  if key_for_min_value{|name_hash|} < 0
  key_for_min_value
 least += 1
 end
