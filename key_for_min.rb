b3d2cc625621aa8d99f32527bdd0d0bd31d6e768
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 00:11:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
-require 'pry'
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  binding.pry
+
   name_hash.first[1]
   current_smallest_value = name_hash.first[1]
   k = name_hash.first[0]
