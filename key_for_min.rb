415b8cabe7e1d2215d836b85224e636ab98db815
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 00:13:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.first[1]
   current_smallest_value = name_hash.first[1]
   k = name_hash.first[0]
-   name_hash.each |key, value|
+   name_hash.each do |key, value| 
    if current_smallest_value > value
      current_smallest_value = value
      k = key
